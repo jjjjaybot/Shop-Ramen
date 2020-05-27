5dcb6b23eebcdb21901c829e9240c2a62f728bf7
Jiazi Cai <caijiazi777@gmail.com>
2020-05-27 14:20:47 -0400

    add lazy load

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react"
 import { ProductConsumer } from "../../../context"
+import LazyLoad from "react-lazyload"
 
 export default class Product extends Component {
   render() {
     const { id, title, img, price, inCart } = this.props.product
+
     return (
       <ProductConsumer>
         {value => (
           <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-3">
             <div className="card" style={{ minHeight: "100%" }}>
-              <img src={img} alt="ramen" className="card-img-top" />
+              <LazyLoad>
+                <img src={img} alt="ramen" className="card-img-top" />
+              </LazyLoad>
               <div className="card-body text-center">
                 <h6>{title}</h6>
                 <h6>${price}</h6>
